4ae71358242f06035b505144b498c0b74b295371
AryanMir15 <aryanmir115@gmail.com>
2026-06-19 12:42:06 +0500

    chore: bump version to 4.9.0 (versionCode 70)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,8 +107,8 @@ android {
         applicationId = "com.lagradost.cloudcache"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 69
-        versionName = "4.8.0"
+        versionCode = 70
+        versionName = "4.9.0"
 
         manifestPlaceholders["target_sdk_version"] = libs.versions.targetSdk.get()
 
