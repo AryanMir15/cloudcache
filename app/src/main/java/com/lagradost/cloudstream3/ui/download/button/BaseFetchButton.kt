cb83e59a3ad747bd6974d267e8e20ca491fff610
Leandre Chamberland-Dozois <lchamberland-dozois@hotmail.com>
2026-07-14 11:19:39 -0400

    fix(download): remove deleted/cancelled items from the Downloads list in real time (#1227) (#2598)

--- app/src/main/java/com/lagradost/cloudstream3/ui/download/button/BaseFetchButton.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/download/button/BaseFetchButton.kt
@@ -168,9 +168,11 @@ abstract class BaseFetchButton(context: Context, attributeSet: AttributeSet) :
         }
     }
 
-    /*fun downloadDeleteEvent(data: Int) {
-
-    }*/
+    fun downloadDeleteEvent(data: Int) {
+        if (data == persistentId) {
+            resetView()
+        }
+    }
 
     /*fun downloadEvent(data: Pair<Int, VideoDownloadManager.DownloadActionType>) {
         val (id, action) = data
@@ -189,7 +191,7 @@ abstract class BaseFetchButton(context: Context, attributeSet: AttributeSet) :
 
     override fun onAttachedToWindow() {
         VideoDownloadManager.downloadStatusEvent += ::downloadStatusEvent
-        // VideoDownloadManager.downloadDeleteEvent += ::downloadDeleteEvent
+        VideoDownloadManager.downloadDeleteEvent += ::downloadDeleteEvent
         // VideoDownloadManager.downloadEvent += ::downloadEvent
         VideoDownloadManager.downloadProgressEvent += ::downloadProgressEvent
 
@@ -204,7 +206,7 @@ abstract class BaseFetchButton(context: Context, attributeSet: AttributeSet) :
 
     override fun onDetachedFromWindow() {
         VideoDownloadManager.downloadStatusEvent -= ::downloadStatusEvent
-        // VideoDownloadManager.downloadDeleteEvent -= ::downloadDeleteEvent
+        VideoDownloadManager.downloadDeleteEvent -= ::downloadDeleteEvent
         // VideoDownloadManager.downloadEvent -= ::downloadEvent
         VideoDownloadManager.downloadProgressEvent -= ::downloadProgressEvent
 
